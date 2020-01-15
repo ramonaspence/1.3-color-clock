11f8605fe6241b104384ab5eeec6e79ab9dc09fc
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-15 15:58:51 -0500

    starting to add basic styling

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+//Immediately Invoked Function Element
+
+(function() {
+  'use strict';
+
+  // let $container = document.querySelector('span');
+  //
+  // $container.textContent = "Friday"
+  //
+  // let $elementId = document.querySelector('#my-id');
+  // console.log($elementId.textContent);
+  //
+  // let $date = new Date(); creates variable $date to equal to the "new Date" which is the time of it running.
+  // console.log(date); prints current time and date in console log
+  //
+  // setInterval(function, ms);
+  // let currentTime;
+  //
+  // let getCurrentTime = () => {
+  //   currentTime = new Date();
+  //   console.log(currentTime);
+  // }
+  //
+  // setInterval(getCurrentTime, 1000);
+  //
+  // let $button = document.querySelector('button');
+  // $button.addEventListener('click', () => {
+  //   console.log('you touched the but');
+  // });
+  //
+  // let $time = document.querySelector('time');
+  // $time.addEventListener('hover', () => {});
+
+  
+
+})();
