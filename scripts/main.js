0a56c1d144b3af214c8658e6006ab61c267dfb5e
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-15 18:14:55 -0500

    declared two variables and got it to return the current time upon reload (with lines 33-38) and got it to repeat the return every one second

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,16 @@
   // let $date = new Date(); creates variable $date to equal to the "new Date" which is the time of it running.
   // console.log(date); prints current time and date in console log
   //
-  // setInterval(function, ms);
-  // let currentTime;
-  //
-  // let getCurrentTime = () => {
-  //   currentTime = new Date();
-  //   console.log(currentTime);
-  // }
-  //
-  // setInterval(getCurrentTime, 1000);
-  //
+  //setInterval(function, ms);
+   // let currentTime;
+   //
+   // let getCurrentTime = () => {
+   //   currentTime = new Date();
+   //   console.log(currentTime);
+   // }
+
+
+
   // let $button = document.querySelector('button');
   // $button.addEventListener('click', () => {
   //   console.log('you touched the but');
@@ -30,7 +30,16 @@
   //
   // let $time = document.querySelector('time');
   // $time.addEventListener('hover', () => {});
+      let currentTime //this declares a variable and allows you to give it value on line36
 
-  let $time = new Date();
+      let getCurrentTime = () => {   //this declares a variable for a function?
+        currentTime = new Date();    //then gives currentTime a value
+        console.log(currentTime)    //then gives it a...method or object and function?
+      }
+
+      // console.log(getCurrentTime);
+
+      setInterval(getCurrentTime, 1000); //sets a time interval to repeat the above function on lines 33-38
+
 
 })();
