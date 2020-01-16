a18f2ae687876eac61a1085c4cbd38a8603784fd
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-16 16:37:56 -0500

    all of what needs to be repeated must be in the function

--- scripts/main.js
+++ scripts/main.js
@@ -12,23 +12,42 @@
   //
   // let $date = new Date(); creates variable $date to equal to the "new Date" which is the time of it running.
   // console.log(date); prints current time and date in console log
+
   let $time = document.querySelector('time');
 
+  let setTime = () => {
+    // console.log('hey');
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let mins = currentTime.getMinutes();
+    let secs = currentTime.getSeconds();
 
-  let $currentTime = new Date();
+    let getCurrentTime = (`${hours}:${mins}:${secs}`);
 
-
-  let $hours = $currentTime.getHours();
-  let $mins = $currentTime.getMinutes();
-  let $secs = $currentTime.getSeconds();
-
-  let getCurrentTime = (`${$hours}:${$mins}:${$secs}`);
-
-  let thisFunction = () => {
     $time.textContent = (getCurrentTime);
-}
 
-  setInterval(thisFunction, 1000);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  setInterval(setTime, 1000);
+
+
+
+
 
 
 })();
